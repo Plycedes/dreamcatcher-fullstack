fac0c2a101c8223c7654ae14c9df29e098ec321d
Plycedes <abhaypratapyadav340@gmail.com>
2024-07-30 18:23:58 +0530

    Changes

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,7 +1,8 @@
 import "@styles/globals.css";
+import Link from "next/link";
 
 export const metadata = {
-    title: "One Piece",
+    title: "Dreamcatcher",
     description: "Decentralized Lottery",
 };
 
@@ -10,6 +11,25 @@ const RootLayout = ({ children }) => {
         <html lang="en">
             <body>
                 <div className="main">
+                    <div>
+                        <nav className="border-b p-6">
+                            <p className="text-4xl font-bold">Dreamcatcher</p>
+                            <div className="flex mt-4">
+                                <Link href="/" className="mr-4 text-pink-500">
+                                    Home
+                                </Link>
+                                <Link href="/create-nft" className="mr-6 text-pink-500">
+                                    Sell NFT
+                                </Link>
+                                <Link href="/my-nfts" className="mr-6 text-pink-500">
+                                    My NFTs
+                                </Link>
+                                <Link href="/dashboard" className="mr-6 text-pink-500">
+                                    Dashboard
+                                </Link>
+                            </div>
+                        </nav>
+                    </div>
                     <main className="app">{children}</main>
                 </div>
             </body>
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,4 +1,21 @@
+"use client";
+
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
+import { nftaddress, nftmarketaddress } from "@config";
+
+import NFT from "D:/Web3/dreamcatcher-fullstack/backend/artifacts/contracts/NFT.sol/NFT.json";
+import Dreamcatcher from "D:/Web3/dreamcatcher-fullstack/backend/artifacts/contracts/Dreamcatcher.sol/Dreamcatcher.json";
+
 const Home = () => {
+    const [nfts, setNfts] = useState([]);
+    const [loadingState, setLoadingState] = useState("not-loaded");
+    useEffect(() => {
+        //loadNFTs();
+    }, []);
     return <div>Test</div>;
 };
 
